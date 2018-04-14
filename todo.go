c4a6109dcb5d92648d962d9e418d6c18a8ecd15b
DataNick <DataNick@users.noreply.github.com>
2018-04-14 02:20:34 -0400

    Created database; successful database connection

--- todo.go
+++ todo.go
@@ -1,17 +1,19 @@
 package main
 
 import (
+  "database/sql"
   //"net/http"
   "github.com/labstack/echo"
-  // "github.com/labstack/echo/engine/standard"
-
-
+  _"github.com/mattn/go-sqlite3"
 )
 
 
 func main(){
-  //A new instance of Echo
 
+  db := initDB("storage.db")
+  migrate(db)
+
+  //A new instance of Echo
   e := echo.New()
 
   e.GET("/tasks", func(c echo.Context) error { return c.JSON(200, "GET Tasks") })
@@ -20,6 +22,37 @@ func main(){
 
   //Start web server
 
-  // e.Run(standard.New(":8000"))
   e.Logger.Fatal(e.Start(":8000"))
+}
+
+func initDB(filepath string) *sql.DB {
+  db, err := sql.Open("sqlite3", filepath)
+
+  // Check for db errors
+
+  if err != nil {
+    panic(err)
+  }
+
+  //  No errors and no db connection --  exit
+  if db == nil {
+    panic("db nil")
+  }
+
+  return db
+}
+
+func migrate(db *sql.DB) {
+  sql := `
+    CREATE TABLE IF NOT EXISTS tasks(
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR NOT NULL
+    );
+    `
+  _, err := db.Exec(sql)
+
+  //Exit if SQL statement above errors out
+  if err != nil {
+    panic(err)
+  }
 }

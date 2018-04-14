d2cc9a2662bd6d4f91318f6a8b0f24ae441f022f
DataNick <DataNick@users.noreply.github.com>
2018-04-13 23:00:15 -0400

    Set up server

--- todo.go
+++ todo.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+  //"net/http"
   "github.com/labstack/echo"
-  "github.com/labstack/echo/engine/standard"
+  // "github.com/labstack/echo/engine/standard"
+
+
 )
 
-func main(){
 
+func main(){
+  //A new instance of Echo
+
+  e := echo.New()
+
+  e.GET("/tasks", func(c echo.Context) error { return c.JSON(200, "GET Tasks") })
+  e.PUT("/tasks", func(c echo.Context) error { return c.JSON(200, "PUT Tasks") })
+  e.DELETE("/tasks/:id", func(c echo.Context) error {return c.JSON(200, "DELETE Task " + c.Param("id"))})
+
+  //Start web server
+
+  // e.Run(standard.New(":8000"))
+  e.Logger.Fatal(e.Start(":8000"))
 }

f5d323399db8a8243b6252c882ebb04de18976a2
DataNick <DataNick@users.noreply.github.com>
2018-04-17 16:47:25 -0400

    Include model calls in handler functions

--- handlers/tasks.go
+++ handlers/tasks.go
@@ -5,6 +5,8 @@ import (
   "net/http"
   "strconv"
 
+  "go-echo-vue/models"
+
   "github.com/labstack/echo"
 )
 
@@ -16,24 +18,53 @@ type H map[string]interface{}
 //GetTasks endpoint
 func GetTasks(db *sql.DB) echo.HandlerFunc {
   return func(c echo.Context) error {
-    return c.JSON(http.StatusOK, "tasks")
+    //return c.JSON(http.StatusOK, "tasks")
+    //Fetch tasks using model
+    return c.JSON(http.StatusOK, models.GetTasks(db))
   }
 }
 
 //PutTask endpoint
 func PutTask(db *sql.DB) echo.HandlerFunc {
   return func(c echo.Context) error {
-    return c.JSON(http.StatusCreated, H{
-      "created": 123,
-      })
+    // return c.JSON(http.StatusCreated, H{
+    //   "created": 123,
+    //   })
+    //Instantiate new task
+    var task models.Task
+    //Map incoming JSON body to the new task
+    c.Bind(&task)
+    //Add task using model
+    id, err := models.PutTask(db, task.Name)
+    //Return JSON response if successfull creation of task
+    if err == nil {
+      return c.JSON(http.StatusCreated, H{
+        "created": id,
+        })
+      //Handle errors
+    } else {
+      return err
+    }
+
   }
 }
 //DeleteTask
 func DeleteTask(db *sql.DB) echo.HandlerFunc {
   return func(c echo.Context) error {
     id, _ := strconv.Atoi(c.Param("id"))
-    return c.JSON(http.StatusOK, H{
-      "deleted": id,
-      })
+    // return c.JSON(http.StatusOK, H{
+    //   "deleted": id,
+    //   })
+    //Use model to delete a task
+    _, err := models.DeleteTask(db, id)
+    //Return JSON response if successful deletion of record
+    if err == nil {
+      return c.JSON(http.StatusOK, H{
+        "deleted": id,
+        })
+      //Handle errors
+    } else {
+      return err
+    }
   }
 }
